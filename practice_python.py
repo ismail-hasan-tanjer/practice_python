898fcd1f17d3ddf0fe3029bcd8ff54299ff01cc1
ismail-hasan-tanjer <tanjerinfo@gmail.com>
2025-02-21 09:43:37 +0600

    variable

--- practice_python.py
+++ practice_python.py
@@ -6,3 +6,14 @@ if 10 > 5:
     print("10 is greater than 5")  
     
     
+# variable 
+
+name = "Tanjer"
+age = 28
+height = 5.4
+is_student = True 
+print (name, age, height, is_student)
+
+
+
+
