a2de44901b5e780f46ac2750328e8cc33b5d2dc8
ismail-hasan-tanjer <tanjerinfo@gmail.com>
2025-03-01 13:23:34 +0600

    python machine learning #Mean Median Mode

--- practice_python.py
+++ practice_python.py
@@ -366,6 +366,18 @@ p2.join()
 
 print("All processes completed!") 
 
+#python machine learning 
+
+#Mean Median Mode 
+
+import numpy
+
+speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+
+x = numpy.mean(speed)
+
+print(x)
+
 
 
 
