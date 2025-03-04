5b56cd69c588f6c3759c6a647cab6e87a7c999d4
ismail-hasan-tanjer <tanjerinfo@gmail.com>
2025-03-05 00:56:06 +0600

    Generators (Memory Efficient Iterators)

--- practice_python.py
+++ practice_python.py
@@ -604,6 +604,18 @@ print (square_lambda)
 square_lambda = lambda x: x + x 
 print (square_lambda(5))
 
+#Generators (Memory Efficient Iterators)
+
+def my_generator():
+    yield 1
+    yield 2
+    yield 3
+
+gen = my_generator()
+print(next(gen))  # Output: 1
+print(next(gen))  # Output: 2
+print(next(gen))  # Output: 3
+
 
 
 
