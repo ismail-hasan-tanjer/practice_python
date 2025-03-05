adf74a8a8ededdaf9a4654a63f9ed8dfb2388c78
ismail-hasan-tanjer <tanjerinfo@gmail.com>
2025-03-05 16:21:32 +0600

    Library Import

--- practice_python.py
+++ practice_python.py
@@ -640,6 +640,16 @@ say_hello()
 
 # pip install scikit-learn
 
+#Library Import 
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error 
+
+
+
 
 
 
